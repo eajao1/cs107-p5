75dddc839e0b134e4ffbd15499509be8766aeb27
Eniola O. Ajao <eniola.ajao@gmail.com>
2018-10-24 17:17:54 -0400

    Finished P5

--- stacks.py
+++ stacks.py
@@ -1,6 +1,7 @@
 # CS 107, Fall 2018 -- Project 5
 # Imperative Data and Stacks
 from library import *
+import doctest
 
 class StackUnderflow(Exception):
     pass
@@ -28,8 +29,14 @@ def size(stack):
 
 # Return a new copy of the stack, without changing anything
 def copyStack(s):
-    return [s[0], s[1] + [], s[2]]
+    precondition(looksLikeStack(s))
+    newArray = [None] * size(s)   #define a newArray, the same size as stack
+    for i in range(size(s)):      #iterate through the old stack, building up newArray
+        newArray[i] = s[1][i]     #take each element from the old stack, and assign it to newArray
+    postcondition(len(newArray) == size(s))
+    return [s[0], newArray, s[2]] #return a stack, with the newArray          
 
+    
 # Push an element onto a stack
 def push(stack, e):
     precondition(looksLikeStack(stack))
@@ -55,15 +62,121 @@ def push(stack, e):
 
 # Pop the top element from the stack
 def pop(stack):
+    
+    newArray = copyStack(stack) #create a newArray that is an exact copy of the stack. 
     precondition(looksLikeStack(stack))
-    raise UnimplementedExeception
 
+    if isEmpty(stack): #raise a StackUnderFlowException if stack has no elements in it
+        raise StackUnderFlowExeception
+    else:
+        #Replace "popped" element (top of stack) with 'None'
+        #Decrease the value representing where the top of the stack is.
+        stack[1][top(stack) - 1] = None
+        stack[0] = top(stack) - 1       
+
+    return newArray[1][top(newArray)-1]   #return the top of the stack
+
+
+#Takes a stack as an argument and returns an array representing the stack as an array.
+#Must remove all unused elements.
 def toArray(stack):
-    raise UnimplementedExeception
+    precondition(looksLikeStack(stack))
 
+    newArray = [None] * stack[0]        #define a newArray, with size of the stack[0]
+    for i in range(top(stack)):         #iterate through the stack
+        if i != None:                   #As long as the element is not unused..
+            newArray[i] = stack[1][i]   #Assign the value to newArray
+    postcondition(type(newArray) == type([]))
+    return newArray                     #return the array
+    
+    
+#This function reverses a stack. The empty space in the array must be left alone.
 def reverse(stack):
-    raise UnimplementedExeception
+    precondition(looksLikeStack(stack))
+    
+    newArray = [None] * stack[2]                     #define a newArray, the same size as the stack
+    for i in range(stack[0]):                        #Note that we iterate in the range of stack[0], as to leave empty space untouched.
+        newArray[i] = stack[1][top(stack) - (i + 1)] #Go "backwards", reassigning each element in the old array to the new array
+    stack[1] = newArray
+    stack = [stack[0], stack[1], stack[2]]           #reassign stack as with newArray
 
+    postcondition(looksLikeStack(stack))
+    return stack
+    
+
+#This is a helper function that counts how many "None"'s occur in
+#an array.
+def countNone(stack):
+    precondition(looksLikeStack(stack))
+    """
+    >>> countNone([3, [1,2,3,None, None, None],6])
+    3
+
+    >>> countNone()
+    0
+
+    >>> countNone([4, [3,6,9,12], 4]
+    0
+
+    >>> countNone([0, [None], 1])
+    1
+
+    """
+    y = 0                 #define a variable to eventually increment. 
+    if isEmpty(stack[0]): #if the stack is empty, return 0. There are no "None"'s.
+        return 0  
+    else:
+        for i in stack[1]: 
+            if i == None: #if the element is a "None"..
+                y = y+1   #increment y by 1
+    postcondition(type(y) == int)
+    return y              #return y
+
+#This function inserts an element e into a list, such that the list remains sorted.
+def insert(lst, e):
+    precondition(type(lst) == type([]))
+    precondition(type(e) == int)
+    
+    """    
+    >>> insert([1,2,None,None], 3)
+    [1, 2, 3, None]
+
+    >>> insert([None], 8)
+    [8]
+
+    >>> insert([], 5)
+    [5]
+    
+    """
+    #Note: This function assumes that there is a "None" in the list,
+    #otherwise a merging won't work anyway, because there won't be space to merge.
+    #It still works if a list does not contain a 'None' though.
+    
+    if isEmpty(lst):            #if the list is empty, return a list containing only e
+        return [e]
+    if head(lst) == None:       #if the head of the list is a 'None'...
+        return [e] + lst[:-1]   #"replace" the None with 'e' by returninf a list of [e] and removing the last element in the list.
+    if head(lst) >= e:          #if the head of the list is greater than e, 
+        return [e] + lst[:-1]   #return a list with e at the front.
+    else: 
+        return [head(lst)] + insert(tail(lst), e) #otherwise, return the head of the list, and recurse on the rest of the list.
+
+#This function merges s1 and s2, using the helper function 'insert.'
+#It works by inserting each element from s2's array into s1's array one at a time.    
 def merge(s1,s2):
-    raise UnimplementedExeception
+    precondition(looksLikeStack(s1))
+    precondition(looksLikeStack(s2))
+    
+    #First, check if s1 is large enough to accommodate the elements from s2:
+    if countNone(s1) < size(s2): #if the amount of 'None's in s1 is less than the size of s2...
+        raise IndexError         #raise an index error
+    else:
+        x = s1[1]                #assign x to the array in stack 1
+        for i in s2[1]:          #iterate through each element in the stack 2's array
+            x = insert(x, i)     #insert i into the array, assign the newly 'sorted' array to x
+        s1[0] = (s1[0]+s2[0])    #For this step, I replace each index in s1 individually to create a new stack.
+        s1[1] = x
+        s1[2] = s1[2]
+        postcondition(looksLikeStack(s1))
+        return s1                #return the altered s1
 
--- tests.py
+++ tests.py
@@ -17,6 +17,7 @@ class TestStacks(unittest.TestCase):
     # Write more tests here
     def test_push(self):
         s = make_stack(2)
+    
         push(s,1)
         self.assertEqual(s[0], 1)
         self.assertEqual(s[1], [1, None])
@@ -30,33 +31,139 @@ class TestStacks(unittest.TestCase):
         self.assertEqual(s[1], [1, 2, 3, None])
         self.assertEqual(s[2], 4)
 
+        #my tests:
+        my_stack = make_stack(4)
+        push(my_stack,1)
+        
+        self.assertEqual(my_stack[0], 1)
+        self.assertEqual(my_stack[1], [1, None, None, None])
+        self.assertEqual(my_stack[2], 4)
+        
+
     def test_copyStackA(self):
         ex1 = [1, [235], 1]
         x = copyStack(ex1)
         self.assertEqual(x[2], 1)
 
+        #my tests:
+        birthday_stack = [3, [11, 27, 1998, None], 4]
+        twin = copyStack(birthday_stack)
+        
+        self.assertEqual(twin[0], 3)
+        self.assertEqual(twin[1], [11, 27, 1998, None])
+        self.assertEqual(twin[2], 4)
+
+
+        my_example = [2, [1, 2, None, None, None, None], 6]
+        copy_example = copyStack(my_example)
+
+        self.assertEqual(copy_example[0], 2)
+        self.assertEqual(copy_example[1], [1, 2, None, None, None, None])
+        self.assertEqual(copy_example[2], 6)
+        
+
+        exampleToo = [3, [10,20,30], 3]
+        copyToo = copyStack(exampleToo)
+
+        self.assertEqual(copyToo[0], 3)
+        self.assertEqual(copyToo[1], [10,20,30])
+        self.assertEqual(copyToo[2], 3)
+        
     def test_copyStackB(self):
         ex1 = [1, [235], 1]
         x = copyStack(ex1)
         self.assertEqual(x[0], 1)
 
+        #my tests:
+
+        #In this series of tests, I am making sure that if I change the array in
+        #the return value from copyStack(s), those changes are not reflected in s.
+        
+        birthday_stack = [3, [11, 27, 1998, None], 4]
+        twin = copyStack(birthday_stack)
+        twin[1][1] = 28
+        self.assertEqual(birthday_stack, [3, [11, 27, 1998, None], 4])
+        
+        my_example = [2, [1, 2, None, None, None, None], 6]
+        copy_example = copyStack(my_example)
+        copy_example[1][4] = 5
+        self.assertEqual(my_example, [2, [1, 2, None, None, None, None], 6])
+
+        exampleToo = [3, [10,20,30], 3]
+        copyToo = copyStack(exampleToo)
+        copyToo[1][2] = 50
+        self.assertEqual(exampleToo, [3, [10,20,30], 3])
+        
+
+        
     def test_pop(self):
         ex1 = [1, [235], 1]
         self.assertEqual(pop(ex1), 235)
 
+        #my tests:
+        birthday_stack = [3, [11, 27, 1998, None], 4]
+        self.assertEqual(pop(birthday_stack), 1998)
+        self.assertEqual(birthday_stack, [2, [11, 27, None, None], 4])
+
+        exampleToo = [3, [10,20,30], 3]
+        self.assertEqual(pop(exampleToo), 30)
+        self.assertEqual(exampleToo, [2, [10,20,None], 3])
+
+        
     def test_toArray(self):
         ex1 = [1, [235], 1]
         self.assertEqual(toArray(ex1),[235])
 
+
+        #my tests:
+        birthday_stack = [3, [11, 27, 1998, None], 4]
+        self.assertEqual(toArray(birthday_stack),[11, 27, 1998])
+
+        my_example = [2, [1, 2, None, None, None, None], 6]
+        self.assertEqual(toArray(my_example),[1, 2])
+        
+        exampleToo = [3, [10,20,30], 3]
+        self.assertEqual(toArray(exampleToo),[10,20,30])
+        
     def test_reverse(self):
         ex1 = [2, [1,2], 2]
         reverse(ex1)
         self.assertEqual(ex1[1], [2,1])
+
+        #my tests:
+        birthday_stack = [3, [11, 27, 1998, None], 4]
+        reverse(birthday_stack)
+        self.assertEqual(birthday_stack[1], [1998,27,11, None])
+        
+        my_example = [2, [1, 2, None, None, None, None], 6]
+        reverse(my_example)
+        self.assertEqual(my_example[1], [2,1,None, None, None, None])
+
+    
+        exampleToo = [3, [10,20,30], 3]
+        reverse(exampleToo)
+        self.assertEqual(exampleToo[1], [30,20,10])
+        self.assertEqual(exampleToo[0], 3)
+        self.assertEqual(exampleToo[2], 3)
 
     def test_merge(self):
         x = [2, [1,3, None, None], 4]
         merge(x,[1,[2],1])
         self.assertEqual(x[1], [1,2,3,None])
         self.assertEqual(x[0], 3)
+
+        #my tests:
+        birthday_stack = [3, [11, 27, 1998, None], 4]
+        merge(birthday_stack,[1,[2],1])
+        self.assertEqual(birthday_stack[1], [2,11,27,1998])
+        self.assertEqual(birthday_stack[0], 4)
+        
+        my_example = [2, [1, 2, None, None, None, None], 6]
+        exampleToo = [3, [10,20,30], 3]
+
+        merge(my_example, exampleToo)
+        self.assertEqual(my_example[1], [1, 2, 10, 20, 30, None])
+        self.assertEqual(my_example[0], 5)
+        
 
 unittest.main()
